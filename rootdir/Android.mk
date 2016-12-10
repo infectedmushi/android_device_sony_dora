e15450f9b1fb96d1a53bc33aa254bb21ebfbc5f5
Adrian DC <radian.dc@gmail.com>
2016-12-10 15:18:25 +0100

    dora: rootdir: Initial ramdisk init and fstab files
     * Based upon various configurations and init files
        from ivy, huashan, SODP, official Nougat 39.2.A.0.361
    
    Change-Id: I91f878712461ff5053b618da40aac36b8a2f7d7e
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

---
+++ rootdir/Android.mk
@@ -0,0 +1,88 @@
+LOCAL_PATH:= $(call my-dir)
+
+# Init scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.$(TARGET_DEVICE)
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.$(TARGET_DEVICE).rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.device.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.qcom.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.recovery.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.sony.usb.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.sony.usb.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.$(TARGET_DEVICE).rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := ueventd.sony.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.fm.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.fm.sh
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.sensors.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.sensors.sh
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init_emmc_15-424A4E423452-0f_bin
+LOCAL_MODULE_STEM  := 15-424A4E423452-0f.bin
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := lib/firmware/emmc/15-424A4E423452-0f.bin
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/lib/firmware/emmc
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init_emmc_catalog_txt
+LOCAL_MODULE_STEM  := catalog.txt
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := lib/firmware/emmc/catalog.txt
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/lib/firmware/emmc
+include $(BUILD_PREBUILT)
+
+$(shell mkdir -p $(PRODUCT_OUT)/root && pushd $(PRODUCT_OUT)/root > /dev/null && \
+    ln -fs fstab.$(TARGET_DEVICE) fstab.qcom && \
+    popd > /dev/null)
+
